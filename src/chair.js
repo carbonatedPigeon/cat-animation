54fbb41f39e17bdb98e0fce43991beaa7d2d0720
Rheannon Franklin <rheannon.franklin@factset.com>
2023-10-27 11:09:09 -0400

    refactoring again

--- src/chair.js
+++ src/chair.js
@@ -35,90 +35,59 @@ class ChairBack extends DrawableItem {
         let chairBackWidthPosition = (this.chairDimensions.chairWidth / 2) - (this.chairDimensions.boxWidth / 2);
         
         //setting dimensions of left/right vertical boxes
-        let verticalBoxDepth = this.chairDimensions.boxWidth;
-        let verticalBoxHeight = this.chairDimensions.backHeight;
-        let verticalBoxWidth = this.chairDimensions.boxDepth
-        this.leftBackVerticalBox.setItemDimensions(
-            verticalBoxDepth, 
-            verticalBoxHeight, 
-            verticalBoxWidth
-        );
-        this.rightBackVerticalBox.setItemDimensions(
-            verticalBoxDepth, 
-            verticalBoxHeight, 
-            verticalBoxWidth
-        );
+        //left
+        this.leftBackVerticalBox.itemDepth = this.chairDimensions.boxWidth;
+        this.leftBackVerticalBox.itemHeight = this.chairDimensions.backHeight;
+        this.leftBackVerticalBox.itemWidth = this.chairDimensions.boxDepth;
+        //right
+        this.rightBackVerticalBox.itemDepth = this.chairDimensions.boxWidth;
+        this.rightBackVerticalBox.itemHeight = this.chairDimensions.backHeight;
+        this.rightBackVerticalBox.itemWidth = this.chairDimensions.boxDepth;
         //setting positions of left/right vertical boxes
-        let backVerticalBoxForwardPosition = chairBackWidthPosition;
-        let backVerticalBoxVerticalPosition = -this.chairDimensions.backHeight / 2;
-        let leftBackVerticalBoxHorizontalPositon = -chairBackDepth;
-        let rightBackVerticalBoxHorizontalPosition = -leftBackVerticalBoxHorizontalPositon
-        this.leftBackVerticalBox.setItemPosition(
-            backVerticalBoxForwardPosition, 
-            backVerticalBoxVerticalPosition, 
-            leftBackVerticalBoxHorizontalPositon,
-        );
-        this.rightBackVerticalBox.setItemPosition(
-            backVerticalBoxForwardPosition, 
-            backVerticalBoxVerticalPosition, 
-            rightBackVerticalBoxHorizontalPosition,
-        );
-        //dimensions of top horizontal box on back of chair
-        let horizontalBoxDepth = this.chairDimensions.boxDepth;
-        let topHorizontalBoxWidth = this.chairDimensions.chairLength + this.chairDimensions.boxWidth; //extending past vertical boxes
-        let horizontalBoxHeight = this.chairDimensions.boxWidth
-        this.topHorizontalBox.setItemDimensions(
-            horizontalBoxDepth,
-            horizontalBoxHeight,
-            topHorizontalBoxWidth,
-        );
-        //dimensions of middle horizontal box on back of chair
-        let middleHorizontalBoxWidth = this.chairDimensions.chairLength - this.chairDimensions.boxWidth;
-        this.middleHorizontalBox.setItemDimensions(
-            horizontalBoxDepth,
-            horizontalBoxHeight,
-            middleHorizontalBoxWidth,
-        );
-        //position of top horizontal box on back of chair
-        let horizontalBoxForwardPosition = -chairBackDepth;
-        let topHorizontalBoxVerticalPosition = -this.chairDimensions.backHeight;
-        let horizontalBoxHorizontalPosition = 0;
-        this.topHorizontalBox.setItemPosition(
-            horizontalBoxForwardPosition, 
-            topHorizontalBoxVerticalPosition, 
-            horizontalBoxHorizontalPosition
-        );
-        //position of middle horizontal box on back of chair
-        let middleHorizontalBoxVerticalPosition = -this.chairDimensions.backHeight / 2;
-        this.middleHorizontalBox.setItemPosition(
-            horizontalBoxForwardPosition, 
-            middleHorizontalBoxVerticalPosition, 
-            horizontalBoxHorizontalPosition
-        );
+        //left
+        this.leftBackVerticalBox.forwardPosition = chairBackWidthPosition;
+        this.leftBackVerticalBox.verticalPosition = -this.chairDimensions.backHeight / 2;
+        this.leftBackVerticalBox.horizontalPosition = -chairBackDepth;
+        //right
+        this.rightBackVerticalBox.verticalPosition = -this.chairDimensions.backHeight / 2;
+        this.rightBackVerticalBox.horizontalPosition = chairBackDepth;
+        this.rightBackVerticalBox.forwardPosition = chairBackWidthPosition;
+        //dimensions of horizontal boxes on back of chair
+        //top
+        this.topHorizontalBox.itemDepth = this.chairDimensions.boxDepth;
+        this.topHorizontalBox.itemHeight = this.chairDimensions.boxWidth;
+        this.topHorizontalBox.itemWidth = this.chairDimensions.chairLength + this.chairDimensions.boxWidth; //extending past vertical boxes
+        //middle
+        this.middleHorizontalBox.itemDepth = this.chairDimensions.boxDepth;
+        this.middleHorizontalBox.itemHeight = this.chairDimensions.boxWidth;
+        this.middleHorizontalBox.itemWidth = this.chairDimensions.chairLength - this.chairDimensions.boxWidth;
+        //position of of horizontal boxes on back of chair
+        //top
+        this.topHorizontalBox.forwardPosition = -chairBackDepth;
+        this.topHorizontalBox.verticalPosition = -this.chairDimensions.backHeight;
+        this.topHorizontalBox.horizontalPosition = 0;
+        //middle
+        this.middleHorizontalBox.forwardPosition = -chairBackDepth;
+        this.middleHorizontalBox.verticalPosition = -this.chairDimensions.backHeight / 2;
+        this.middleHorizontalBox.horizontalPosition = 0;
     }
 }
 
-class ChairCusion {
+class ChairCusion extends BoxComponent {
     constructor(chairDimensions) {
+        super();
         //default color is white
         this.itemColor = color(255);
         this.position = createVector(
-        0,
-        -chairDimensions.boxWidth,
-        (chairDimensions.chairWidth * 0.1 / 2) - chairDimensions.boxWidth
+            0,
+            -chairDimensions.boxWidth,
+            (chairDimensions.chairWidth * 0.1 / 2) - chairDimensions.boxWidth
         );
         this.dimensions = createVector(
         chairDimensions.chairWidth - (chairDimensions.chairWidth * 0.1), 
         chairDimensions.boxWidth, 
         chairDimensions.chairDepth - (chairDimensions.chairWidth * 0.1)
         );
-    }
-    drawItem() {
-        fill(this.itemColor)
-        push();
-        translate(this.position);
-        box(this.dimensions.x, this.dimensions.y, this.dimensions.z);
-        pop();
     }
 }
 
