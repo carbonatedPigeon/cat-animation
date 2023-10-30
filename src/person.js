56afa78c88efaa9e4e6376ad4f04b46821a7d967
Rheannon Franklin <rheannon.franklin@factset.com>
2023-10-30 09:29:03 -0400

    refactoring person components

--- src/person.js
+++ src/person.js
@@ -2,27 +2,19 @@ class Person extends ComponentAmalgamation {
   constructor() {
     super();
     //storing list of components that make up person
-    this.eyeBrows = new Eyebrows();
-    this.eyes = new Eyes();
     this.head = new Head();
-    this.eyelashes = new Eyelashes();
-    this.hair = new Hair();
     this.body = new Body();
     this.legs = new Legs();
     this.components = [
-      this.eyeBrows,
-      this.eyes,
       this.head,
-      this.eyelashes,
-      this.hair,
       this.body,
       this.legs,
     ];
   }
   setSkinColor(skinColor) {
     //setting skin color of head & legs
-    this.head.headColor = this.skinColor;
-    this.legs.legColor = this.skinColor;
+    this.head.headColor = skinColor;
+    this.legs.legColor = skinColor;
   }
   setHairColor(hairColor) {
     //setting hair color of hair, eyelashes, and eyebrows
@@ -32,8 +24,9 @@ class Person extends ComponentAmalgamation {
   }
 }
 
-class Eyebrows {
+class Eyebrows extends ComponentAmalgamation {
   constructor() {
+    super();
     //default eyebrow color == black
     this.eyebrowColor = (0, 0, 0);
     this.leftEyebrowAngle = 0;
@@ -42,6 +35,13 @@ class Eyebrows {
     this.rightEyebrowPosition = createVector(-22, -15, 60);
     this.leftEyebrowSize = createVector(15, 5, 10);
     this.rightEyebrowSize = createVector(15, 5, 10);
+
+    this.leftEyebrow = new Eyebrow(22, 20, 0);
+    this.rightEyebrow = new Eyebrow(-22, -20, 0);
+    this.components = [
+      this.leftEyebrow,
+      this.rightEyebrow,
+    ];
   }
   
   drawItem() {
@@ -60,6 +60,27 @@ class Eyebrows {
     rotateZ(this.rightEyebrowAngle);
     box(this.rightEyebrowSize.x, this.rightEyebrowSize.y, this.rightEyebrowSize.z);
     pop();
+  }
+
+  setYRotation(yRotation) {
+    this.leftEyebrow.yRotation = yRotation;
+    this.rightEyebrow.yRotation = -yRotation;
+  }
+
+  setZRotation(zRotation) {
+    this.leftEyebrow.zRotation = zRotation;
+    this.rightEyebrow.zRotation = -zRotation;
+  }
+}
+
+class Eyebrow extends BoxComponent {
+  constructor(horizontalPosition, yRotation, zRotation) {
+    super();
+    this.yRotation = yRotation;
+    this.zRotation = zRotation;
+    this.forwardPosition = horizontalPosition;
+    this.horizontalPosition = 60;
+    this.forwardPosition = -15;
   }
 }
 
@@ -110,6 +131,48 @@ class Eyes {
     }
 }
 
+class Eyes2 extends ComponentAmalgamation {
+  constructor() {
+    this.leftEye = new Eye(0);
+    this.rightEye = new Eye(0);
+    this.components = [
+      this.leftEye,
+      this.rightEye,
+    ];
+  }
+}
+
+class Eye extends ComponentAmalgamation {
+  constructor(horizontalPosition) {
+    super();
+    this.horizontalPosition = horizontalPosition;
+    this.pupil = new Pupil();
+    this.components = [
+      new Sclera(),
+      this.pupil,
+      new Eyelash(),
+    ];
+  }
+}
+
+class Sclera extends SphereComponent {
+  constructor() {
+    super();
+  }
+}
+
+class Pupil extends SphereComponent {
+  constructor() {
+    super();
+  }
+}
+
+class Eyelash extends CustomShapeComponent {
+  constructor() {
+    super();
+  }
+}
+
 class Eyelashes {
   constructor() {
     //eyelash variables
@@ -134,92 +197,94 @@ class Eyelashes {
   }
 }
 
-class Head {
+class Head extends ComponentAmalgamation {
   constructor() {
-    this.headSize = 50;
-    this.headScale = createVector(1.2, 1.5, 1.2);
-    this.headColor = color(255, 255, 225);
-  }
-  
-  drawItem() {
-    fill(this.headColor);
-    push();
-    scale(this.headScale);
-    sphere(this.headSize);
-    pop();
+    super();
+    this.eyes = new Eyes();
+    this.eyelashes = new Eyelashes();
+    this.eyebrows = new Eyebrows();
+    this.headShape = new HeadShape();
+    this.hair = new Hair();
+    this.components = [
+      this.eyes,
+      this.eyebrows,
+      this.eyelashes,
+      this.headShape,
+      this.hair,
+    ]
   }
 }
 
-class Hair {
+class HeadShape extends SphereComponent {
   constructor() {
-    this.hairColor = color(0);
-    this.hairPosition = createVector(0, -10, -7);
-    this.hairScale = createVector(1.5, 1.5, 1.2);
-    this.hairSize = 50;
-  }
-  drawItem() {
-    fill(this.hairColor);
-    push();
-    translate(this.hairPosition);
-    scale(this.hairScale);
-    sphere(this.hairSize);
-    pop();
+    super();
+    this.size = 50;
+    this.itemScale = createVector(1.2, 1.5, 1.2);
+    this.itemColor = color(255, 255, 225);
   }
 }
 
-class Body {
+class Hair extends SphereComponent {
   constructor() {
-    //color of body is black
-    this.bodyColor = color(0);
-    this.bodyPosition = createVector(0, 220, 0);
-    this.bodyScale = createVector(1.5, 2, 1.2);
-    this.bodySize = 75;
-  }
-  
-  drawItem() {
-    fill(this.bodyColor);
-    push();
-    translate(this.bodyPosition);
-    scale(this.bodyScale);
-    sphere(this.bodySize);
-    pop();
+    super();
+    this.size = 50;
+    this.itemScale = createVector(1.5, 1.5, 1.2);
+    this.verticalPosition = -10;
+    this.horizontalPosition = -7;
   }
 }
 
-class Arms {
+class Body extends SphereComponent {
   constructor() {
-    
-  }
-  drawItem() {
-    
+    super();
+    this.verticalPosition = 220;
+    this.itemScale = createVector(1.5, 2, 1.2);
+    this.size = 75;
   }
 }
 
-class Legs  {
+class Arms extends ComponentAmalgamation {
   constructor() {
-    self.legColor = color(255, 255, 225);
-    self.leftLegPosition = createVector(75, 300, 100);
-    self.rightLegPosition = createVector(-75, 300, 100);
-    self.leftLegRotateX = 90;
-    self.rightLegRotateX = 90;
-    self.leftLegRotateZ = -15;
-    self.rightLegRotateZ = 15;
-    self.legSize = createVector(25, 125);
+    super();
+    this.leftArm = new Arm();
+    this.rightArm = new Arm();
+    this.components = [
+      this.leftArm,
+      this.rightArm,
+    ];
   }
-  drawItem() {
-    fill(self.legColor);
-    push();
-    translate(self.leftLegPosition);
-    rotateX(self.leftLegRotateX);
-    rotateZ(self.leftLegRotateZ);
-    cylinder(self.legSize.x, self.legSize.y);
-    pop();
+}
 
-    push();
-    translate(self.rightLegPosition);
-    rotateX(self.rightLegRotateX);
-    rotateZ( self.rightLegRotateZ);
-    cylinder(self.legSize.x, self.legSize.y);
-    pop();
+class Arm extends CylinderComponent {
+  constructor() {
+    super();
+  }
+}
+
+class Legs extends ComponentAmalgamation {
+  constructor() {
+    super();
+    this.legColor = color(255, 255, 225);
+    this.leftLeg = new Leg(75, -15);
+    this.rightLeg = new Leg(-75, 15);
+    this.leftLeg.forwardPosition = 75;
+    this.components = [
+      this.leftLeg,
+      this.rightLeg,
+    ]
+  }
+}
+
+class Leg extends CylinderComponent {
+  constructor(forwardPosition, zRotation) {
+    super();
+    this.itemColor = color(255, 255, 225);
+    this.radius = 25;
+    this.itemHeight = 125;
+    this.xRotation = 90;
+    this.forwardPosition = forwardPosition;
+    this.verticalPosition = 300;
+    this.horizontalPosition = 100;
+    this.zRotation = zRotation;
   }
 }
